b0e816d345e9ffe067b86f3d5151d2a7daf4326e
santoshc4610 <santoshc4610@gmail.com>
2022-01-29 01:40:08 +0530

    Moving Linked last into lab package

---
+++ src/main/java/RemoveLoopFromLinkedList.java
@@ -0,0 +1,12 @@
+import com.example.lab.LinkedList;
+
+public class RemoveLoopFromLinkedList
+{
+    /** This program is to remove a loop from a linked list **/
+
+    LinkedList list1 = new LinkedList();
+    
+
+
+
+}
--- src/main/java/com/example/lab/LinkedList.java
+++ src/main/java/com/example/lab/LinkedList.java
@@ -1,3 +1,5 @@
+package com.example.lab;
+
 import java.util.Scanner;
 
 public class LinkedList {
