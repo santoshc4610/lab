f0e5572a49b0b7d780a5a121c4552373d6fed6ff
santoshc4610 <santoshc4610@gmail.com>
2022-01-29 02:25:52 +0530

    Update RemoveLoopFromLinkedList.java

--- src/main/java/RemoveLoopFromLinkedList.java
+++ src/main/java/RemoveLoopFromLinkedList.java
@@ -1,5 +1,7 @@
 import com.example.lab.CustomLinkedList;
 import com.example.lab.CustomLinkedList.Node;
+
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import java.util.ArrayList;
@@ -9,19 +11,21 @@ public class RemoveLoopFromLinkedList
     /** This program is to remove a loop from a linked list **/
 
     public static void main(String Args[]) {
-        LinkedList list2 = new LinkedList();
+        LinkedList<Integer> list2 = new LinkedList<>();
         ArrayList<Integer> arr1 = new ArrayList<>();
-        Node head;
-
-
-
-
         list2.add(15);
         list2.add(25);
         list2.add(30);
         list2.add(43);
 
-        System.out.println(list2.getFirst());
+    for (int i = 0 ; i < list2.size(); i ++)
+
+         System.out.println(list2.get(i));
+
+
+
+
+
 
 
         }
